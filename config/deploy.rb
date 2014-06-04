b6482463881f8eebf8798e25f45a7eeec13a86ff
ubuntu <ubuntu@ubuntu.(none)>
2014-06-04 14:17:22 -0700

    Capistrano configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for Capistrano 3.1
-lock '3.1.0'
+#lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'bw_webblog'
+set :repo_url, 'git@github.com:benweible/bw_webblog.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
